4559467d2bc20ef0608334d76f41cd004ca8144f
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2025-01-24 18:03:29 +0900

    6_Express(multer, pug, nunjucks)

--- 6_Express/app.js
+++ 6_Express/app.js
@@ -4,10 +4,21 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const dotenv = require("dotenv");
 const path = require("path");
+const nunjucks = require("nunjucks");
 
 dotenv.config();
+const indexRouter = require("./routes");
+const userRouter = require("./routes/user");
+
 const app = express();
 app.set("port", process.env.PORT || 3000);
+// app.set("views", path.join(__dirname, "view"));
+app.set("view engine", "html");
+
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
 
 app.use(morgan("dev"));
 app.use("/", express.static(path.join(__dirname, "public")));
@@ -25,6 +36,50 @@ app.use(
     },
     name: "session-cookie",
   })
+);
+
+app.use("/", indexRouter);
+app.use("/user", userRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+const multer = require("multer");
+const fs = require("fs");
+
+try {
+  fs.readdirSync("uploads");
+} catch (err) {
+  console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 + 1024 },
+});
+
+app.get("/upload", (req, res) => {
+  res.sendFile(path.join(__dirname, "multipart2.html"));
+});
+app.post(
+  "/upload",
+  upload.fields([{ name: "image1" }, { name: "image2" }]),
+  (req, res) => {
+    console.log(req.file, req.body);
+    res.send("ok");
+  }
 );
 
 app.use((req, res, next) => {
@@ -46,8 +101,10 @@ app.get(
 );
 
 app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send(err.message);
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 app.listen(app.get("port"), () => {
