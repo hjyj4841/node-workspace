f6e70becc74db5eaca4118a4bdf63030c633ccbb
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2025-02-01 18:20:54 +0900

    9_SNS_Project(login 구현 중)

--- 9_SNS_Project/app.js
+++ 9_SNS_Project/app.js
@@ -5,13 +5,15 @@ const path = require("path");
 const session = require("express-session");
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
+const passport = require("./passport");
 
 dotenv.config();
 const pageRouter = require("./routes/page");
-
 const { sequelize } = require("./models");
+const passportConfig = require("./passport");
 
 const app = express();
+passportConfig(); // 패스포트 설정
 app.set("port", process.env.PORT || 8001);
 app.set("view engine", "html");
 nunjucks.configure("views", {
@@ -44,6 +46,8 @@ app.use(
     },
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
 // 라우터
 app.use("/", pageRouter);
